582e3c64bec223639f66039477f3f8885e119180
Justin Widney <jwidney@cs.ualberta.ca>
2019-03-07 23:25:49 -0700

    assigment is done.

--- server.py
+++ server.py
@@ -22,7 +22,8 @@
 
 
 import flask
-from flask import Flask, request
+import os
+from flask import Flask, redirect, request, Response, jsonify, render_template
 import json
 app = Flask(__name__)
 app.debug = True
@@ -74,27 +75,39 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", 301)
+
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+
+    json_post_data = flask_post_json()
+    for key,val in json_post_data.items():
+        myWorld.update(entity,key,val)
+    
+
+
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+
+
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+
+    return jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
